21b6a1535a577eb3528cf6b29b277845b6e8be28
maple7422 <ben0827@gmail.com>
2017-01-27 02:12:00 +0800

    Dev 1.0
    開發用來合併104履歷使用

---
+++ merge_104.py
@@ -0,0 +1,38 @@
+﻿import xlrd
+import xlwt
+import os
+
+new_save_file_name = '104_merge.xls'
+new_rows = 1
+
+file = xlwt.Workbook()
+newtable= file.add_sheet('104',cell_overwrite_ok=True)
+
+filelist = os.listdir(os.getcwd())
+for y in range(0,len(filelist)):
+    if (filelist[y] == new_save_file_name):
+        os.remove(new_save_file_name)
+filelist = os.listdir(os.getcwd())
+for y in range(0,len(filelist)):
+    if (filelist[y].find('xls') > 0):
+            print('正在處理 ' + filelist[y])
+            data = xlrd.open_workbook(filelist[y])
+            table = data.sheets()[0]
+            nrows = table.nrows
+            ncols = table.ncols
+
+            for i in range(0,ncols):
+                newtable.write(0,i,table.row_values(1)[i])
+            for int_nrows in range(2,nrows):
+                for i in range(0,ncols):
+                    newtable.write(new_rows,i,table.row_values(int_nrows)[i])
+                new_rows = new_rows + 1
+print('建立 ' + new_save_file_name)
+file.save(new_save_file_name)
+print('完成')
+
+
+
+
+
+
